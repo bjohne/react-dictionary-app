1e1701c0c81aed881726d948998e2fa2758f7d42
bjohne <97284793+bjohne@users.noreply.github.com>
2023-02-28 11:02:51 +0100

    built search-engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded{" "}
+          <a
+            href="https://github.com/bjohne/react-dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-source
+          </a>{" "}
+          by Betina Johne
+        </footer>
+      </div>
     </div>
   );
 }
