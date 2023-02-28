f6307da344822589559e58ef3bf76336bcfc6982
bjohne <97284793+bjohne@users.noreply.github.com>
2023-02-28 11:34:05 +0100

    added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`searching for ${keyword}`);
+
+    let apiKey = `31596ta47a643ofdbb992da3f1ed09dc`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateKeyword(event) {
