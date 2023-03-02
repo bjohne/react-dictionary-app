b888399abced159aa5fbea7d47aace8611b9a8c5
bjohne <97284793+bjohne@users.noreply.github.com>
2023-03-02 17:29:10 +0100

    styled dictionary, meaning, synonyms

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,33 +1,53 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data);
   }
 
-  function search(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-
-    let apiKey = `31596ta47a643ofdbb992da3f1ed09dc`;
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
   }
 
   function updateKeyword(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={updateKeyword} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function search() {
+    let apiKey = `31596ta47a643ofdbb992da3f1ed09dc`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              defaultValue={props.defaultKeyword}
+              onChange={updateKeyword}
+            />
+          </form>
+          <div className="hint">i.E. sunset, joy, Paris....</div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    search();
+  }
 }
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,13 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <p>
-        {" "}
-        <br />
-        <strong>{props.meaning.partOfSpeech}</strong>
-      </p>
-      <div> {props.meaning.definition} </div>
-      <em> {props.meaning.example} </em>
-
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <div className="definition"> {props.meaning.definition} </div>
+      <div className="example"> {props.meaning.example} </div>
       <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
