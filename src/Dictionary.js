92a832a757a71111f129d038f613249f716bd9d1
bjohne <97284793+bjohne@users.noreply.github.com>
2023-03-04 11:01:54 +0100

    added images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data);
   }
 
@@ -20,12 +22,20 @@ export default function Dictionary(props) {
   function updateKeyword(event) {
     setKeyword(event.target.value);
   }
+  function handleImagesResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
+  }
 
   function search() {
     let apiKey = `31596ta47a643ofdbb992da3f1ed09dc`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(imagesApiUrl).then(handleImagesResponse);
   }
+
   function load() {
     setLoaded(true);
     search();
@@ -34,6 +44,7 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
+          <h3>Which word would you like to look up?</h3>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
@@ -41,9 +52,11 @@ export default function Dictionary(props) {
               onChange={updateKeyword}
             />
           </form>
+
           <div className="hint">i.E. sunset, joy, Paris....</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
